70c819bc9069b194c72a02cf6ddbb2b89ec9cec9
ReLive27 <re_live27@163.com>
2022-11-13 16:52:28 +0800

    Delete clounm link

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
-  { href: '/columns', title: 'Column' },
+  // { href: '/columns', title: 'Column' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
 ]
